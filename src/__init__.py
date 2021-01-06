6ccba22a0421e276759684d2af0149dad9bb8582
Nick Hafkemeyer <mail@nhafkemeyer.com>
2021-01-06 17:09:04 +0100

    fail silently when instance config missing

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_object('config')
-        app.config.from_pyfile('config.py')
+        app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
